64f041554737e448a9f4dca88ebe22bb8c730fb7
Sourav Basu <sourav24@gmail.com>
2025-06-26 15:40:38 -0700

    Update __init__.py

--- custom_components/acwd_water_usage/__init__.py
+++ custom_components/acwd_water_usage/__init__.py
@@ -3,11 +3,10 @@ from .const import DOMAIN
 
 async def async_setup(hass, config):
     """Set up the ACWD Water Usage component."""
-    return await _dry_setup(hass, config)
-
+    return True
+    
 async def async_setup_entry(hass, config_entry):
     """Set up ACWD Water Usage from a config entry."""
-    res = await _dry_setup(hass, entry.data)
     return await hass.config_entries.async_forward_entry_setups(config_entry, "sensor")
 
 async def async_unload_entry(hass, config_entry):
